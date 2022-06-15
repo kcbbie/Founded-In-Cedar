c09b78d690db8da1c2d8282c9f61889cdcd358e5
kcbbie <67881281+kcbbie@users.noreply.github.com>
2022-06-14 22:37:15 -0700

    Add files via upload

---
+++ globals/WordType.java
@@ -0,0 +1,12 @@
+package globals;
+
+/**
+ * All the (current) possible types of words recognized in the game.
+ */
+public enum WordType {
+    VERB,
+    NOUN,
+    PREPOSITION,
+    ARTICLE,
+    DIRECTION
+}
